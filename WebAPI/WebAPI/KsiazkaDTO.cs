84df5782f41a596dcd29d9afb9c293d741ef3b2d
Sebastian Repko <sr305432@student.polsl.pl>
2025-06-12 12:38:14 +0200

    Update KsiazkaDTO.cs

--- WebAPI/WebAPI/KsiazkaDTO.cs
+++ WebAPI/WebAPI/KsiazkaDTO.cs
@@ -1,11 +1,22 @@
-﻿namespace WebAPI
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI
 {
     public class KsiazkaDto
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Tytul { get; set; }
-        public string Autor { get; set; }
+
+        [Required]
+        [RegularExpression("^(?i)(fantasy|romans|dystopia)$")]
         public string Gatunek { get; set; }
+
+        [Range(0, 2025)]
         public int Rok { get; set; }
+
+        public string Autor { get; set; }
     }
 }
